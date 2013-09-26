e1e4d04e3a5631303cb2d6eef734609747d32cf7
Tomohiro Nishimura <tomohiro68@gmail.com>
2013-09-26 21:57:57 +0900

    initial commit

---
+++ Assets/Keyboard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Keyboard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default, true, false, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
